9455f5f868f50931838fd116419b313cd1174202
Ludovic FAVIER <lfavier@live.fr>
2022-02-13 15:08:48 +0100

    add instructions pre-processors for windows and mac

--- src/Views/Draw/Draw.cpp
+++ src/Views/Draw/Draw.cpp
@@ -5,6 +5,10 @@
 #include "Draw.h"
 #include <iostream>
 
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
 const bool IS_NOT_QUITING_DRAW = false;
 const bool IS_QUITING_DRAW = true;
 
@@ -22,5 +26,11 @@ void Draw::launch() {
 }
 
 void Draw::loadDrawing() {
+    #ifdef _WIN32
+    ShellExecute(NULL, "open", "http://dreamincode.net",
+                NULL, NULL, SW_SHOWNORMAL);
+    #else
     system("open ../Ressources/Drawings/test.svg");
+    #endif
+
 }
