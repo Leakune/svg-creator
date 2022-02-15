93178554c96a80c02891ed49f4d3eaa049f43be5
Ludovic FAVIER <lfavier@live.fr>
2022-02-15 22:54:34 +0100

    rename MenuNameFile

---
+++ src/Views/MenuNameFile/MenuNameFile.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Ludovic FAVIER on 14/02/2022.
+//
+
+#include "MenuNameFile.h"
+#include <iostream>
+
+void MenuNameFile::askNameFile(){
+}
+
+MenuNameFile::MenuNameFile() {
+
+}
+
+
---
+++ src/Views/MenuNameFile/MenuNameFile.h
@@ -0,0 +1,16 @@
+//
+// Created by Ludovic FAVIER on 14/02/2022.
+//
+
+#ifndef SVG_CREATOR_NAMEFILE_H
+#define SVG_CREATOR_NAMEFILE_H
+#include <string>
+
+class MenuNameFile {
+    public:
+    MenuNameFile();
+    void askNameFile();
+};
+
+
+#endif //SVG_CREATOR_NAMEFILE_H
